e8d340e797918a56abc81f9f57f49531a9b80f8d
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-22 16:31:10 -0300

    Requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
-  // });
+  it('O retorno de fetchProductsList é igual ao retorno de computadorSearch', async () => {
+    await expect(fetchProductsList('computador')).toBe(computadorSearch());
+  });
+
+  it('O retorno vazio de fetchProductsList reproduz o erro: Termo de busca não informado', async () => {
+    await expect(fetchProductsList()).rejects.toThrow(
+      new Error('Termo de busca não informado')
+      );
+    });    
 });
