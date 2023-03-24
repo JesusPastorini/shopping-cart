ac713b860db9689f623afe7b99a062a794666cc9
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-24 02:52:38 -0300

    requisito 4

--- src/main.js
+++ src/main.js
@@ -6,9 +6,22 @@ import './style.css';
 const exibeProdutos = async () => {
     const products = document.querySelector('.products');
     const api = await fetchProductsList('computador');
+    const load = document.querySelector('.loading');
     api.forEach((element) => { 
         products.appendChild(createProductElement(element));
-    });
+    });  
+    load.parentElement.removeChild(load);
 }; 
-exibeProdutos();
+
+const carregando = () => {
+    const products = document.querySelector('.products');
+    const loading = document.createElement('span');
+    loading.className = 'loading';
+    products.appendChild(loading);
+    loading.innerHTML = 'Carregando...';
+};
+carregando();
+window.onload = function onload() {
+    exibeProdutos()
+};
 document.querySelector('.cep-button').addEventListener('click', searchCep);
